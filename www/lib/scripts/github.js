e2457a6e8b7c89ffe4c81675f15d6101bf348711
Mr.Fan <noobfan@email.com>
2015-12-22 23:25:54 +0800

    fan

---
+++ www/lib/scripts/github.js
@@ -0,0 +1,111 @@
+(
+    function (root, factory) {
+        root.Github = factory(root);
+    }(this, function (root) {
+        var Base64Encode = function (str) {
+            var base64encode;
+            if (typeof define === 'function' && define.amd) {
+                define(['js-base64'], function (encode) {
+                    base64encode = encode.Base64.encode;
+                });
+            } else if (typeof module === 'object' && module.exports) {
+                if (typeof window !== 'undefined') {
+                    base64encode = window.btoa;
+                } else {
+                    base64encode = require('js-base64').Base64.encode;
+                }
+            } else {
+                base64encode = function (str) {
+                    return root.btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
+                        return String.fromCharCode('0x' + p1);
+                    }));
+                };
+            }
+            return base64encode(str);
+        };
+
+        var APIURL = 'https://api.github.com';
+        var APIVERSION = 'v3';
+        var Github = function (option) {
+            var getURL = function (path) {
+                var url = path.indexOf('//') >= 0 ? path : APIURL + path;
+                url += ((/\?/).test(url) ? '&' : '?');
+                return url.replace(/(&timestamp=\d+)/, '') +
+                    (typeof window !== 'undefined' ? '&timestamp=' + new Date().getTime() : '');
+            }
+            var getParams = function (params) {
+                var p;
+                if (params && typeof params === 'object' && ['GET', 'HEAD', 'DELETE'].indexOf(method) > -1) {
+                    for (var param in params) {
+                        if (params.hasOwnProperty(param))
+                            p += '&' + encodeURIComponent(param) + '=' + encodeURIComponent(params[param]);
+                    }
+                }
+                return p;
+            }
+            var request = function (method, path, params, callback, mediaType, async) {
+                $.ajax(
+                    {
+                        type: method,
+                        beforeSend: function (xhr) {
+                            var type = mediaType ? '.' + mediaType : '';
+                            var accept = 'application/vnd.github.' + APIVERSION + type + '+json';
+                            xhr.setRequestHeader('Accept', accept);
+                            //xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+                            var authorization = 'Basic ' + Base64Encode(option.username + ':' + option.password);
+                            xhr.setRequestHeader('Authorization', authorization);
+                        },
+                        //data: params,
+                        url: getURL(path),
+                        data: getParams(params),
+                        dataType: "json",
+                        async: async,
+                        success: function (data, textStatus) {
+                            if (callback)
+                                callback(data);
+                        },
+                        error: function (XMLHttpRequest, textStatus, errorThrown) {
+                            if (callback)
+                                callback(textStatus + " : " + errorThrown);
+                        },
+                    }
+                );
+            };
+
+            this.show = function (username, callback) {
+                var path = username ? '/users/' + username : '/user';
+                request('GET', path, null, callback);
+            };
+
+            this.Repositories = function () {
+                return new function () {
+                    this.listPersonalRepos = function (params, callback) {
+                        var path = '/user/repos';
+                        request('GET', path, params, callback);
+                    }
+                    this.listPublicRepos = function (username, params, callback) {
+                        var path = '/users/' + username + '/repos';
+                        request('GET', path, params, callback);
+                    }
+
+                    this.listOrganizationRepos = function (orgname, params, callback) {
+                        var path = '/orgs/' + orgname + '/repos';
+                        request('GET', path, params, callback);
+                    }
+
+                    this.createPersonalRepo = function (params, callback) {
+                        var path = '/user/repos';
+                        request('POST', path, params, callback);
+                    }
+                    this.createOrganizationRepo = function (orgname, params, callback) {
+                        var path = '/orgs/' + orgname + '/repos';
+                        request('POST', path, params, callback);
+                    }
+                }
+            };
+
+            return this;
+        };
+        return Github;
+    })
+)
--- www/lib/scripts/personal.js
+++ www/lib/scripts/personal.js
@@ -1,98 +1,96 @@
-
 jQuery.resizeView = function (id) {
-	if(id)
-	    $(function(){
-	        if(window.parent!=window) {
-	            var winH = $(window).height();
-	            var bodyH = $(document).height();
-	            if (bodyH > winH) {
-	                window.parent.document.getElementById(id).height = bodyH;
-	            } else {
-	                window.parent.document.getElementById(id).height = winH;
-	            }
-	        }
-	    });
+    if (id)
+        $(function () {
+            if (window.parent != window) {
+                var winH = $(window).height();
+                var bodyH = $(document).height();
+                if (bodyH > winH) {
+                    window.parent.document.getElementById(id).height = bodyH;
+                } else {
+                    window.parent.document.getElementById(id).height = winH;
+                }
+            }
+        });
 };
 
-jQuery.imgLoaded=function(callback){
-    var imgs=document.getElementsByTagName("img");
-    for(var i=0;i<imgs.length;i++){
-        imgs[i].addEventListener("load",callback);
+jQuery.imgLoaded = function (callback) {
+    var imgs = document.getElementsByTagName("img");
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].addEventListener("load", callback);
     }
 }
-jQuery.readText=function(file,callback,async){
-    if(file){
+jQuery.readText = function (file, callback, async) {
+    if (file) {
         //Fun.debug("load file:"+file);
-        $(function() {
+        $(function () {
             $.ajax({
                 url: file,
                 dataType: 'text',
-                async: async?true:false ,
-                success: function(data, textStatus){
-                //    Fun.debug("load file done:"+file);
+                async: async ? true : false,
+                success: function (data, textStatus) {
+                    //    Fun.debug("load file done:"+file);
                     callback(data);
                 },
-                error:function(XMLHttpRequest, textStatus, errorThrown){
-                    Fun.debug("load file error:"+errorThrown);
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    Fun.debug("load file error:" + errorThrown);
                 }
             });
         });
-    }else{
+    } else {
         //Fun.debug("file undefine");
     }
 };
 
-jQuery.loadMD=function(fromFile,toElementID,iframe){
-	jQuery.readText(fromFile,function(data){
-        var content= document.getElementById(toElementID);
-        var html=markdown.toHTML(data);
-        content.innerHTML=html;
-        $('pre code').each(function(i, block) {
+jQuery.loadMD = function (fromFile, toElementID, iframe) {
+    jQuery.readText(fromFile, function (data) {
+        var content = document.getElementById(toElementID);
+        var html = markdown.toHTML(data);
+        content.innerHTML = html;
+        $('pre code').each(function (i, block) {
             hljs.highlightBlock(block);
-         });
-        $('p code').each(function(i, block) {
-        	  hljs.highlightBlock(block);
-        	});
-        
-        jQuery.imgLoaded(function(){
+        });
+        $('p code').each(function (i, block) {
+            hljs.highlightBlock(block);
+        });
+
+        jQuery.imgLoaded(function () {
             jQuery.resizeView(iframe)
         });
 
         jQuery.resizeView(iframe);
-    },false);
+    }, false);
 };
 
 
-var Fun={};
-Fun.debug=function(log){
-   // return;
+var Fun = {};
+Fun.debug = function (log) {
+    // return;
     console.log(log);
 };
 
-Fun.setArgs = function(key,value){
+Fun.setArgs = function (key, value) {
     var p = [];
-    p.push( key + '=' + value);
+    p.push(key + '=' + value);
     return encodeURI('?' + p.join('&'));
 };
 
-Fun.getArgs = function(key) {
+Fun.getArgs = function (key) {
     var retval;
     var params = location.search;
     if (!params) {
         return;
     }
-    var Args   = params.split("?");
-    if(Args[0] == params){
+    var Args = params.split("?");
+    if (Args[0] == params) {
         return;
     }
     var str = Args[1];
     var args = str.split("&");
-    for(var i = 0; i < args.length; i ++)
-    {
+    for (var i = 0; i < args.length; i++) {
         str = args[i];
         var arg = str.split("=");
-        if(arg.length <= 1) continue;
-        if(arg[0] == key){
+        if (arg.length <= 1) continue;
+        if (arg[0] == key) {
             retval = arg[1];
             break;
         }
@@ -100,7 +98,7 @@ Fun.getArgs = function(key) {
     return retval;
 };
 
-Fun.replaceHtml=function(s){
+Fun.replaceHtml = function (s) {
     s = s.replace(/>/g, "&gt;");
     s = s.replace(/</g, "&lt;");
     s = s.replace(/ /g, " &nbsp;");
@@ -109,3 +107,27 @@ Fun.replaceHtml=function(s){
     s = s.replace(/\n/g, "<br/>");
     return s;
 };
+
+
+Fun.Markdown2HtmlByGithub = function (params, callback) {
+    var path = '/markdown' + params.raw ? "/raw" : '';
+    if (params.text) {
+        convert(params.text);
+    } else if (params.file) {
+        jQuery.readText(fromFile, function (data) {
+            convert(params.data);
+        }, true);
+    }
+    var convert = function (txt) {
+        $.ajax({
+            type: 'POST',
+            url: path,
+            dataType: 'text',
+            async: params.async ? params.async : true,
+            success: function (data, textStatus) {
+                //    Fun.debug("load file done:"+file);
+                callback(data);
+            },
+        });
+    }
+}
